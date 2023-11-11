020e311009651db6f82974b62268ec698a3a0bf5
LauzeDao <96375489+LauzeDao@users.noreply.github.com>
2023-11-11 17:03:42 +0100

    Update Forms.js

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,49 +1,52 @@
 import React from "react";
+import "./Form.css";
 
-function Form() {
-    function sendForm() {
-        const firstnameInput = document.getElementById("firstname");
-        const lastnameInput = document.getElementById("lastname");
-        const emailInput = document.getElementById("email");
+function Forms() {
+  function sendForm(event) {
+    event.preventDefault(); // Prevent default form submission
 
-        const firstname = firstnameInput.value;
-        const lastname = lastnameInput.value;
-        const email = emailInput.value;
+    const firstnameInput = document.getElementById("firstname");
+    const lastnameInput = document.getElementById("lastname");
+    const emailInput = document.getElementById("email");
 
-        fetch("https://ddos-bot.azure-api.net/manual/paths/invoke", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Ocp-Apim-Subscription-Key': '7eff73552e9f41eca8c700cbcb241d35'
-            },
-            body: {
-                "name": lastname,
-                "firstname": firstname,
-                "email": email
-            },
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              console.log('Response from server:', data);
-            })
-            .catch((error) => {
-              console.error('Error sending form data:', error);
-            });
-    }
+    const firstname = firstnameInput.value;
+    const lastname = lastnameInput.value;
+    const email = emailInput.value;
 
-    return(
-        <>
-            <div className="formContainer">
-                <form className="userForm">
-                    <input type="text" inputMode="text" id="lastname" placeholder="Name*" required></input>
-                    <input type="text" inputMode="text" id="firstname" placeholder="Vorname*" required></input>
-                    <input type="text" inputMode="email" id="email" placeholder="Email*" required></input>
-                
-                    <button type="submit" onSubmit={sendForm}>Absenden</button>
-                </form>
-            </div>
-        </>
-    );
+    fetch("https://ddos-bot.azure-api.net/manual/paths/invoke", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': '7eff73552e9f41eca8c700cbcb241d35',
+      },
+      body: JSON.stringify({ // Convert the body to JSON
+        "name": lastname,
+        "firstname": firstname,
+        "email": email
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Response from server:', data);
+      })
+      .catch((error) => {
+        console.error('Error sending form data:', error);
+      });
+  }
+
+  return (
+    <>
+      <div className="formContainer">
+        <form className="userForm" onSubmit={sendForm}> {/* Attached onSubmit event to form */}
+          <input type="text" inputMode="text" id="lastname" placeholder="Name*" required />
+          <input type="text" inputMode="text" id="firstname" placeholder="Vorname*" required />
+          <input type="text" inputMode="email" id="email" placeholder="Email*" required />
+
+          <button type="submit">Absenden</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default Form;
+export default Forms;
